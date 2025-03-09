7c0daeb9966e2a2b222867d5a3c3f8e551af9775
Yan iK0da <kodadeveloper@proton.me>
2025-03-09 21:09:02 +0000

    chore: connect Flask to MongoDB

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 from flask import Flask
+from pymongo import MongoClient
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
+
+MONGO_URI = os.getenv("MONGO_URI")
+client = MongoClient(MONGO_URI)
+db = client["todo_list"]
 
 @app.route("/")
 def home():
