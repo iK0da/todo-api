fc46655ae33b0a0e6a45683ab2104928f03407aa
Yan iK0da <kodadeveloper@proton.me>
2025-04-17 21:14:31 +0100

    fix: datetime

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from pymongo import MongoClient
+from datetime import datetime
 import os
 from dotenv import load_dotenv
 
@@ -17,17 +18,24 @@ tasks_collection = db["tasks"]
 @app.route("/tasks", methods=["POST"])
 def add_task():
     data = request.json
-
-    if "date" not in data or "task" not in data:
-        return jsonify({"error": "Date and task are required."}), 400
     
+    if "date" not in data or "task" not in data:
+        return jsonify({"error": "Date and task are required"}), 400
+
+    # Verifica se a data tem apenas o dia (ex: "23")
+    if data["date"].isdigit() and len(data["date"]) <= 2:
+        today = datetime.today()
+        formatted_date = f"{data['date'].zfill(2)}/{today.month:02d}/{today.year}"
+    else:
+        formatted_date = data["date"]  # Usa a data como está
+
     new_task = {
-        "date": data["date"],
+        "date": formatted_date,
         "task": data["task"],
         "status": "pending"
     }
     tasks_collection.insert_one(new_task)
-    return jsonify({"message": "Task added sucessfully"}), 201
+    return jsonify({"message": "Task added successfully"}), 201
 
 @app.route("/tasks", methods=["GET"])
 def get_tasks():
